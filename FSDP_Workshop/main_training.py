4e4b161dd534e5ddf95b813531212a62a351e799
lchu <lchu@us.ibm.com>
2022-09-06 14:36:26 -0400

    modify config to trigger new run in ocp

--- FSDP_Workshop/main_training.py
+++ FSDP_Workshop/main_training.py
@@ -419,7 +419,7 @@ def fsdp_main(args):
         backward_prefetch = model_backward_prefetch,
         cpu_offload=CPUOffload(cpu_offload),
         device_id=torch.cuda.current_device() if not cpu_offload else None,  # streaming init
-        limit_all_gather=True,
+        limit_all_gathers=True,
         inflight_max=2
     )
 
