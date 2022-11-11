6906431caf005db0d58e221d52a9dbd8ce9aa6ca
lchu <lchu@us.ibm.com>
2022-11-11 11:48:50 -0500

    fix ddp_loss calc

--- FSDP_Workshop/main_training.py
+++ FSDP_Workshop/main_training.py
@@ -262,7 +262,7 @@ def validation(model, local_rank, rank, world_size, test_loader):
                 labels=batch["target_ids"],
             )
             ddp_loss[0] += output["loss"].item()  # sum up batch loss
-            ddp_loss[1] += len(batch)
+            ddp_loss[1] += 1
 
             if rank == 0:
                 inner_pbar.update(1)
