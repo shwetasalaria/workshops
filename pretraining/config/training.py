47ff4042086c0826c526df7739b87e61b76f34b3
Davis Wertheimer <dww78@cornell.edu>
2023-10-31 16:01:23 -0400

    Full dataset

--- pretraining/config/training.py
+++ pretraining/config/training.py
@@ -13,16 +13,40 @@ class train_config:
     tokenizer: str = "/lustre/llama_weights/tokenizer.model"
 
     # data and dataloader
-    data_path: str = "/home/bvaughan/"
+    data_path: str = "/lustre/bluepile-processing/rel0_5/tokens_llama2/lang=en/"
     seq_length: int = 4096
     sep_token: int = 0
     datasets = [
         "dataset=commoncrawl",
+        "dataset=webhose",
+        "dataset=github_clean",
+        "dataset=wikipedia/lang=de",
+        "dataset=wikipedia/lang=es",
+        "dataset=wikipedia/lang=fr",
+        "dataset=wikipedia/lang=ja",
+        "dataset=wikipedia/lang=pt",
         "dataset=wikimedia",
+        "dataset=uspto",
+        "dataset=pubmedcentral",
+        "dataset=arxiv",
+        "dataset=stackexchange",
+        "dataset=PG19",
     ]
     weights = [
-        .9,
-        .1,
+        7700,
+        500,
+        550,
+        28,
+        17,
+        22,
+        25,
+        8,
+        100,
+        500,
+        175,
+        250,
+        100,
+        25
     ]
     logical_shards: int = 768
 
@@ -47,8 +71,8 @@ class train_config:
 
     # training spec
     batch_size: int = 2
-    num_epochs: int = 100
-    num_steps: int = 1000
+    num_epochs: int = 1
+    num_steps: int = 250000
     learning_rate: float = 3e-4
 
     # reporting
